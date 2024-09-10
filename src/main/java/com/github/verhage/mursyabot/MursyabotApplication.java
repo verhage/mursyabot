cb68fda6f7f5f5073196e750cabd0d408ab7fb1c
Rens Verhage <rensverhage@gmail.com>
2024-09-10 20:40:08 +0200

    chore: initial project setup

---
+++ src/main/java/com/github/verhage/mursyabot/MursyabotApplication.java
@@ -0,0 +1,11 @@
+package com.github.verhage.mursyabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MursyabotApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MursyabotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/verhage/mursyabot/MursyabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.verhage.mursyabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MursyabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
