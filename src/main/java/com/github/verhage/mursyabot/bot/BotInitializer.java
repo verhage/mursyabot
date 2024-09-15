42dea9eabca72f3f7af7eee9bbfaae0d42b0dada
Rens Verhage <rensverhage@gmail.com>
2024-09-15 17:54:48 +0200

    chore: enabled spring-dev-tools, stop bot session when bean unloads

--- src/main/java/com/github/verhage/mursyabot/bot/BotInitializer.java
+++ src/main/java/com/github/verhage/mursyabot/bot/BotInitializer.java
@@ -1,11 +1,13 @@
 package com.github.verhage.mursyabot.bot;
 
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Slf4j
@@ -15,13 +17,22 @@ public class BotInitializer implements CommandLineRunner {
 
     private final Bot bot;
 
+    private BotSession session;
+
     @Override
     public void run(String... args) {
         try {
             TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
-            api.registerBot(bot);
+            session = api.registerBot(bot);
         } catch (TelegramApiException e) {
             log.error("An unexpected error occurred: {}", e.getMessage(), e);
+        }
+    }
+
+    @PreDestroy
+    public void onDestroy() {
+        if (session != null) {
+            session.stop();
         }
     }
 }
